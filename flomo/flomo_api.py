03efba55f96b2bd887bc7daee9f80b9d50b68bea
ruihe <ruihe>
2025-05-22 14:12:43 +0800

    refactor(flomo_api): 添加调试信息以跟踪latest_updated_at值
    在get_memo_list方法中添加print语句，用于调试和跟踪latest_updated_at的值，以便更好地理解其变化情况。

--- flomo/flomo_api.py
+++ flomo/flomo_api.py
@@ -28,6 +28,7 @@ class FlomoApi:
         current_timestamp = int(time.time())
 
         latest_updated_at = str(int(latest_updated_at) + 1)
+        print(f'get_memo_list latest_updated_at:{latest_updated_at}')
 
         # 构造参数
         params = {
